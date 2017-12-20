555a2b072aa4b807d9fdd2add3e74752dfab8b24
Owen Wilkins <owenwilkins@users.noreply.github.com>
2017-12-20 17:01:31 -0500

    Add files via upload

---
+++ Survival_analyses_of_TCGA_expression/Scripts/TCGA_multivariable_expression_survival_analysis.R
@@ -0,0 +1,210 @@
+
+###########################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Test association between survival time in HNSCC TCGA subjects and tumnor tissue gene expression of all 4 genes 
+# (mir100, mir1251b, let7a2, BLID) derived from the long non-coding RNA locus MIR100HG
+# Authors: Owen Wilkins 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###########################
+rm(list = ls())
+setwd("/Users/Owen/Thesis/HNSCC_miRSNP/")
+library(survival)
+library(survminer)
+
+#------------------------------------------------------------------------------------------
+# load data
+#------------------------------------------------------------------------------------------
+covs_miRNA_oral <- readRDS("05_TCGA_expression_survival_analyses/Data_files/TCGA_oral_cancer_covs_cleaned_w_MIR100HG_expression.rds")
+
+# covariate file for BLID contains a different no of subjects as this is the subset of subjects with available RNA-seq data 
+covs_RNA_oral <- readRDS("05_TCGA_expression_survival_analyses/Data_files/TCGA_oral_cancer_covs_cleaned_w_BLID_expression.rds")
+covs_RNA_larynx <- readRDS("05_TCGA_expression_survival_analyses/Data_files/TCGA_oral_cancer_covs_cleaned_w_SH3BP4_expression.rds")
+
+#------------------------------------------------------------------------------------------
+# fit multivariable cox models for each gene at MIR100HG locus 
+#------------------------------------------------------------------------------------------
+
+#~~~~~~~~~~~~~~~~~~
+# mir-100
+#~~~~~~~~~~~~~~~~~~
+covs <- covs_miRNA_oral
+# fit model w/ mir_100 as continuous variable 
+fit_cont <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                    covs$mir_100_continuous 
+                  + covs$age_at_initial_pathologic_diagnosis 
+                  + covs$gender 
+                  + covs$race_list
+                  + covs$tumor_stage)
+summary(fit_cont)
+
+# build results table 
+results <- matrix(NA, nrow = 5, ncol = 8)
+colnames(results) <- c("Un_P", "Un_HR", "Un_95_CI_LB", "Un_95_CI_UB", 
+                              "Ad_P", "Ad_HR", "Ad_95_CI_LB", "Ad_95_CI_UB")
+rownames(results) <- c("mir100", "let7a2", "mir125b1", "blid", "sh3bp4")
+
+# fill for adjusted model 
+results[1, "Ad_P"] <- round(summary(fit_cont)$coef[1,5], digits = 8)
+results[1, "Ad_HR"] <- round(exp(summary(fit_cont)$coef[1,1]), digits = 8)
+results[1, "Ad_95_CI_LB"] <- round(exp(confint.default(fit_cont))[1, ][1], digits = 8)
+results[1, "Ad_95_CI_UB"] <- round(exp(confint.default(fit_cont))[1, ][2], digits = 8)
+
+# fit unadjusted model w/ mir_100 as categorial variable 
+fit_con_unadj <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                         covs$mir_100_continuous)
+
+# fill for unadjusted model
+results[1, "Un_P"] <- round(summary(fit_con_unadj)$coef[1,5], digits = 8)
+results[1, "Un_HR"] <- round(exp(summary(fit_con_unadj)$coef[1,1]), digits = 8)
+results[1, "Un_95_CI_LB"] <- round(exp(confint.default(fit_con_unadj))[1, ][1], digits = 8)
+results[1, "Un_95_CI_UB"] <- round(exp(confint.default(fit_con_unadj))[1, ][2], digits = 8)
+rm(fit_cont)
+
+#~~~~~~~~~~~~~~~~~~
+# let7a2
+#~~~~~~~~~~~~~~~~~~
+
+# fit model w/ mir_100 as continuous variable 
+fit_cont <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                    covs$let7a_2_continuous 
+                  + covs$age_at_initial_pathologic_diagnosis 
+                  + covs$gender 
+                  + covs$race_list
+                  + covs$tumor_stage)
+summary(fit_cont)
+
+# fill for adjusted model 
+results[2, "Ad_P"] <- round(summary(fit_cont)$coef[1,5], digits = 8)
+results[2, "Ad_HR"] <- round(exp(summary(fit_cont)$coef[1,1]), digits = 8)
+results[2, "Ad_95_CI_LB"] <- round(exp(confint.default(fit_cont))[1, ][1], digits = 8)
+results[2, "Ad_95_CI_UB"] <- round(exp(confint.default(fit_cont))[1, ][2], digits = 8)
+
+# fit unadjusted model w/ mir_100 as categorial variable 
+fit_con_unadj <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                         covs$let7a_2_continuous)
+
+# fill for unadjusted model
+results[2, "Un_P"] <- round(summary(fit_con_unadj)$coef[1,5], digits = 8)
+results[2, "Un_HR"] <- round(exp(summary(fit_con_unadj)$coef[1,1]), digits = 8)
+results[2, "Un_95_CI_LB"] <- round(exp(confint.default(fit_con_unadj))[1, ][1], digits = 8)
+results[2, "Un_95_CI_UB"] <- round(exp(confint.default(fit_con_unadj))[1, ][2], digits = 8)
+rm(fit_cont)
+
+#~~~~~~~~~~~~~~~~~~
+# mir-125b-1
+#~~~~~~~~~~~~~~~~~~
+
+# fit model w/ mir_100 as continuous variable 
+fit_cont <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                    covs$mir125b_1_continuous 
+                  + covs$age_at_initial_pathologic_diagnosis 
+                  + covs$gender 
+                  + covs$race_list
+                  + covs$tumor_stage)
+summary(fit_cont)
+
+# fill for adjusted model 
+results[3, "Ad_P"] <- round(summary(fit_cont)$coef[1,5], digits = 8)
+results[3, "Ad_HR"] <- round(exp(summary(fit_cont)$coef[1,1]), digits = 8)
+results[3, "Ad_95_CI_LB"] <- round(exp(confint.default(fit_cont))[1, ][1], digits = 8)
+results[3, "Ad_95_CI_UB"] <- round(exp(confint.default(fit_cont))[1, ][2], digits = 8)
+
+# fit unadjusted model w/ mir_100 as categorial variable 
+fit_con_unadj <- coxph(Surv(covs$time_to_event, covs$vital_status_2) ~ 
+                         covs$mir125b_1_continuous)
+
+# fill for unadjusted model
+results[3, "Un_P"] <- round(summary(fit_con_unadj)$coef[1,5], digits = 8)
+results[3, "Un_HR"] <- round(exp(summary(fit_con_unadj)$coef[1,1]), digits = 8)
+results[3, "Un_95_CI_LB"] <- round(exp(confint.default(fit_con_unadj))[1, ][1], digits = 8)
+results[3, "Un_95_CI_UB"] <- round(exp(confint.default(fit_con_unadj))[1, ][2], digits = 8)
+rm(fit_cont)
+
+#~~~~~~~~~~~~~~~~~~
+# BLID
+#~~~~~~~~~~~~~~~~~~
+covs_RNA_oral$vital_status_2 <- as.numeric(covs_RNA_oral$vital_status=="Dead")
+# fit model w/ mir_100 as continuous variable 
+fit_cont <- coxph(Surv(covs_RNA_oral$time_to_event, covs_RNA_oral$vital_status_2) ~ 
+                    covs_RNA_oral$blid_continuous 
+                  + covs_RNA_oral$age_at_initial_pathologic_diagnosis 
+                  + covs_RNA_oral$gender 
+                  + covs_RNA_oral$race_list
+                  + covs_RNA_oral$tumor_stage)
+summary(fit_cont)
+
+# fill for adjusted model 
+results[4, "Ad_P"] <- round(summary(fit_cont)$coef[1,5], digits = 8)
+results[4, "Ad_HR"] <- round(exp(summary(fit_cont)$coef[1,1]), digits = 8)
+results[4, "Ad_95_CI_LB"] <- round(exp(confint.default(fit_cont))[1, ][1], digits = 8)
+results[4, "Ad_95_CI_UB"] <- round(exp(confint.default(fit_cont))[1, ][2], digits = 8)
+
+# fit unadjusted model w/ mir_100 as categorial variable 
+fit_con_unadj <- coxph(Surv(covs_RNA_oral$time_to_event, covs_RNA_oral$vital_status_2) ~ 
+                         covs_RNA_oral$blid_continuous)
+summary(fit_con_unadj)
+
+# fill for unadjusted model
+results[4, "Un_P"] <- round(summary(fit_con_unadj)$coef[1,5], digits = 8)
+results[4, "Un_HR"] <- round(exp(summary(fit_con_unadj)$coef[1,1]), digits = 8)
+results[4, "Un_95_CI_LB"] <- round(exp(confint.default(fit_con_unadj))[1, ][1], digits = 8)
+results[4, "Un_95_CI_UB"] <- round(exp(confint.default(fit_con_unadj))[1, ][2], digits = 8)
+
+#~~~~~~~~~~~~~~~~~~
+# SH3BP4
+#~~~~~~~~~~~~~~~~~~
+covs_RNA_larynx$vital_status_2 <- as.numeric(covs_RNA_larynx$vital_status=="Dead")
+# fit model w/ mir_100 as continuous variable 
+fit_cont <- coxph(Surv(covs_RNA_larynx$time_to_event, covs_RNA_larynx$vital_status_2) ~ 
+                    covs_RNA_larynx$sh3bp4_continuous 
+                  + covs_RNA_larynx$age_at_initial_pathologic_diagnosis 
+                  + covs_RNA_larynx$gender 
+                  + covs_RNA_larynx$race_list
+                  + covs_RNA_larynx$tumor_stage)
+summary(fit_cont)
+
+# fill for adjusted model 
+results[5, "Ad_P"] <- round(summary(fit_cont)$coef[1,5], digits = 8)
+results[5, "Ad_HR"] <- round(exp(summary(fit_cont)$coef[1,1]), digits = 8)
+results[5, "Ad_95_CI_LB"] <- round(exp(confint.default(fit_cont))[1, ][1], digits = 8)
+results[5, "Ad_95_CI_UB"] <- round(exp(confint.default(fit_cont))[1, ][2], digits = 8)
+
+# fit unadjusted model w/ mir_100 as categorial variable 
+fit_con_unadj <- coxph(Surv(covs_RNA_larynx$time_to_event, covs_RNA_larynx$vital_status_2) ~ 
+                         covs_RNA_larynx$sh3bp4_continuous)
+summary(fit_con_unadj)
+
+
+perc.rank <- ecdf(covs_RNA_larynx$sh3bp4_continuous)
+percentile <- sapply(covs_RNA_larynx$sh3bp4_continuous, perc.rank)
+names(percentile) <- covs_RNA_larynx$bcr_patient_uuid
+
+# index top 8% and bottom 8% of mir_100 expressors 
+sh3bp4_G1 <- percentile[percentile >= (1-0.5)]
+sh3bp4_G2 <- percentile[percentile >= (1-(1)) & percentile < (1-0.5)]
+
+# add indicator variable to covs to assign subjects to mir_100 expression groups 
+covs_RNA_larynx$sh3bp4_grouped <- NA
+covs_RNA_larynx$sh3bp4_grouped[covs_RNA_larynx$bcr_patient_uuid %in% names(sh3bp4_G1)]  <- "G1"
+covs_RNA_larynx$sh3bp4_grouped[covs_RNA_larynx$bcr_patient_uuid %in% names(sh3bp4_G2)]  <- "G2"
+covs_RNA_larynx$sh3bp4_grouped
+covs_RNA_larynx$sh3bp4_grouped <- as.factor(covs_RNA_larynx$sh3bp4_grouped)
+levels(covs_RNA_larynx$sh3bp4_grouped)
+
+fit_grouped <- coxph(Surv(covs_RNA_larynx$time_to_event, covs_RNA_larynx$vital_status_2) ~ 
+                    covs_RNA_larynx$sh3bp4_grouped 
+                  + covs_RNA_larynx$age_at_initial_pathologic_diagnosis 
+                  + covs_RNA_larynx$gender 
+                  + covs_RNA_larynx$race_list
+                  + covs_RNA_larynx$tumor_stage)
+summary(fit_grouped)
+
+# fill for unadjusted model
+results[5, "Un_P"] <- round(summary(fit_con_unadj)$coef[1,5], digits = 8)
+results[5, "Un_HR"] <- round(exp(summary(fit_con_unadj)$coef[1,1]), digits = 8)
+results[5, "Un_95_CI_LB"] <- round(exp(confint.default(fit_con_unadj))[1, ][1], digits = 8)
+results[5, "Un_95_CI_UB"] <- round(exp(confint.default(fit_con_unadj))[1, ][2], digits = 8)
+
+# save results 
+write.csv(results, '05_TCGA_expression_survival_analyses/Data_files/TCGA_coxph_model_results_expression.csv')
+
---
+++ Survival_analyses_of_TCGA_expression/Scripts/model_building_for_laryngeal_expression_survival_analyses.R
@@ -0,0 +1,148 @@
+###########################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# TCHA HNSCC covariate data cleaning & model building for multivariable survival analyses including TCGA exp. data 
+# Authors: Owen Wilkins 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###########################
+rm(list = ls())
+setwd("/Users/Owen/Thesis/HNSCC_miRSNP/")
+
+#------------------------------------------------------------------------------------------
+# read data
+#------------------------------------------------------------------------------------------
+
+# covariate data 
+covs <- readRDS("05_TCGA_expression_survival_analyses/Data_files/TCGA_HNSC_covariates_miRNA_expression_subset.rds")
+covs$bcr_patient_uuid <- tolower(covs$bcr_patient_uuid)
+
+#------------------------------------------------------------------------------------------
+# clean covariates of interest & explore other variables 
+#------------------------------------------------------------------------------------------
+
+# index for laryngeal tumors only 
+table(covs$anatomic_neoplasm_subdivision)
+covs_larynx <- covs[covs$anatomic_neoplasm_subdivision == "Larynx",]
+
+# determine if there is enough data to adjust for smoking, drinking and hpv status in data set 
+table(is.na(covs_larynx$amount_of_alcohol_consumption_per_day)) 
+table(is.na(covs_larynx$number_pack_years_smoked)) 
+table(covs_larynx$hpv_status_by_p16_testing) 
+# conclusion: too much missingness to adjust for any here 
+
+# consider other variables for inclusion 
+table(covs_larynx$postoperative_rx_tx)
+table(covs_larynx$hpv_status_by_ish_testing)
+table(covs_larynx$drugs)
+table(covs_larynx$stage_event)
+table(covs_larynx$neoplasm_histologic_grade)
+table(covs_larynx$stage_event)
+table(covs_larynx$radiation_therapy)
+
+# CONCLUSION: variables for consideration in multivariable cox model: age, sex, race, tumor stage 
+
+#------------------------------------------------------------------------------------------
+# model building 
+#------------------------------------------------------------------------------------------
+# check univariate associatins of variables 
+summary(coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status=="Dead") ~ covs_larynx$age_at_initial_pathologic_diagnosis))
+# age not strongly associated (will still want to include this, as we know age is often a predictor of survival)
+summary(coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status=="Dead") ~ as.factor(covs_larynx$gender)))
+# sex is strongly associated 
+summary(coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status=="Dead") ~ as.factor(covs_larynx$race_list)))
+# race not strongly associated 
+summary(coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status=="Dead") ~ as.factor(covs_larynx$tumor_stage)))
+# stage not strongly associated 
+
+# make indicator variable numeric so the coxph object made can be used in anova function 
+covs_larynx$vital_status_2 <- as.numeric(covs_larynx$vital_status=="Dead")
+
+#~~~~~~~~~~~~~~~~~~
+# leave one out model building approach
+#~~~~~~~~~~~~~~~~~~
+
+# fit multivariable model 
+fit_1 <- coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status_2) ~ 
+                 covs_larynx$age_at_initial_pathologic_diagnosis + 
+                 covs_larynx$gender + 
+                 covs_larynx$race_list + 
+                 covs_larynx$tumor_stage)
+summary(fit_1)
+# sex and race associated 
+
+# remove sex
+fit_2 <- coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status_2) ~ 
+                 covs_larynx$age_at_initial_pathologic_diagnosis + 
+                 #covs_larynx$gender + 
+                 covs_larynx$race_list + 
+                 covs_larynx$tumor_stage)
+summary(fit_2)
+# race associated 
+
+# remove race
+fit_3 <- coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status_2) ~ 
+                 covs_larynx$age_at_initial_pathologic_diagnosis + 
+                 as.factor(covs_larynx$gender) + 
+                 #as.factor(covs_larynx$race_list) + 
+                 as.factor(covs_larynx$tumor_stage))
+summary(fit_3)
+# sex associated 
+
+fit_4 <- coxph(Surv(covs_larynx$time_to_event, covs_larynx$vital_status_2) ~ 
+                 covs_larynx$age_at_initial_pathologic_diagnosis + 
+                 as.factor(covs_larynx$gender) + 
+                 as.factor(covs_larynx$race_list)) 
+#as.factor(covs_larynx$tumor_stage))
+summary(fit_4)
+# sex associated 
+
+
+#~~~~~~~~~~~~~~~~~~
+# -2loglik testing  
+#~~~~~~~~~~~~~~~~~~
+# the smaller the value of -2loglik, the better the model explains the observed data 
+# if a variable increases -2loglik when ommitted, the variable likely contributes to the outcome
+# so use this approach to identify variables that we should keep in the model  
+
+-2*fit_1$loglik[2]
+-2*fit_2$loglik[2]
+# omition of sex increases -2loglik so reduces model fit, therefore we should keep sex
+
+-2*fit_3$loglik[2]
+# omition of race increases -2loglik so reduces model fit, therefore we want to keep race 
+
+-2*fit_4$loglik[2]
+# omition of stage increases -2loglik so reduces model fit, therefore we want to keep stage 
+
+# test if there is a significant change in this loglik between models 
+anova(fit_2, fit_1) # P = 0.02
+anova(fit_3, fit_1) # P = 0.11
+anova(fit_4, fit_1) # P = 0.41
+# exclusion of sex, race and stage all increase the -2loglik but only the increases in age and race are statistically 
+# significant according to liklihood ratio test 
+
+#~~~~~~~~~~~~~~~~~~
+# test for correlation between variables  
+#~~~~~~~~~~~~~~~~~~
+# consider any association between variables
+
+# age vs sex
+kruskal.test(covs_larynx$age_at_initial_pathologic_diagnosis ~ 
+               as.numeric(covs_larynx$gender == "FEMALE")) # significant 
+# age vs race
+kruskal.test(covs_larynx$age_at_initial_pathologic_diagnosis ~ 
+               as.numeric(covs_larynx$race_list == "NON-WHITE")) # P = 0.42
+# age vs stage
+kruskal.test(covs_larynx$age_at_initial_pathologic_diagnosis ~ 
+               as.numeric(covs_larynx$tumor_stage == "III+IV")) # P = 0.38
+# sex vs race
+chisq.test(matrix(table(as.numeric(covs_larynx$gender == "FEMALE"), 
+                        as.numeric(covs_larynx$race_list == "NON-WHITE")), ncol = 2), simulate.p.value = TRUE)$p.value # non-significant 
+# sex vs stage
+chisq.test(matrix(table(as.numeric(covs_larynx$gender == "FEMALE"), 
+                        as.numeric(covs_larynx$tumor_stage == "III+IV")), ncol = 2), simulate.p.value = TRUE)$p.value # non-significant 
+# stage vs race 
+chisq.test(matrix(table(as.numeric(covs_larynx$tumor_stage == "III+IV"), 
+                        as.numeric(covs_larynx$race_list == "NON-WHITE")), ncol = 2), simulate.p.value = TRUE)$p.value # non-significant 
+# note need to use simulate.p.value as values in some cells for chisq tests are <=5 so chisq approx. may be 
+# inaccurate so need to apply fisher test
+
